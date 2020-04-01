f844a120c0131b3eb8db6aa32e21f3db793ccc8a
jnovikov <johnnovikov0@gmail.com>
2020-04-01 18:04:12 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var (
+	recursiveFlag = flag.Bool("r", false, "recursive search: for directories")
+)
+
+type ScanResult struct {
+	file       string
+	lineNumber int
+	line       string
+}
+
+func scanFile(fpath, pattern string) ([]string, error) {
+	f, err := os.Open(fpath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+	result := make([]string, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, pattern) {
+			result = append(result, line)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func exit(format string, val ...interface{}) {
+	if len(val) == 0 {
+		fmt.Println(format)
+	} else {
+		fmt.Printf(format, val)
+		fmt.Println()
+	}
+	os.Exit(1)
+}
+
+func processFile(fpath string, pattern string) {
+	res, err := scanFile(fpath, pattern)
+	if err != nil {
+		exit("Error scanning %s: %s", fpath, err.Error())
+	}
+	for _, line := range res {
+		fmt.Println(line)
+	}
+}
+
+func processDirectory(dir string, pattern string) {
+
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		exit("usage: go-search <path> <pattern> to search")
+	}
+
+	path := flag.Arg(0)
+	pattern := flag.Arg(1)
+
+	info, err := os.Stat(path)
+	if err != nil {
+		panic(err)
+	}
+
+	recursive := *recursiveFlag
+	if info.IsDir() && !recursive {
+		exit("%s: is a directory", info.Name())
+	}
+
+	if info.IsDir() && recursive {
+		processDirectory(path, pattern)
+	} else {
+		processFile(path, pattern)
+	}
+}
